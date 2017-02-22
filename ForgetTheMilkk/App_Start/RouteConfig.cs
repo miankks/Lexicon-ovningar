e6e37a3f6e14efdba63d1f5c553fba87d5058715
miankks <bilal.jan085@gmail.com>
2017-02-22 13:52:18 +0100

    first update

--- ForgetTheMilkk/App_Start/RouteConfig.cs
+++ ForgetTheMilkk/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ForgetTheMilkk
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Task", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ForgetTheMilkk/Controllers/TaskController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForgetTheMilkk.Controllers
+{
+    public class TaskController : Controller
+    {
+        public static readonly List<string> Tasks = new List<string>();
+        public ActionResult Index()
+        {
+            return View(Tasks);
+        }
+
+
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+            Tasks.Add(task);
+            return RedirectToAction("Index");
+        }
+    }
+}
